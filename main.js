5ab8902c2bea3c1a2bb51a2973d25cd8ff34f5a6
Sumitsingh6923 <singh.sumit.9623@gmail.com>
2025-07-02 21:10:05 +0530

    Add Initial Files

---
+++ main.js
@@ -0,0 +1,30 @@
+Array.from(document.getElementsByTagName('input')).forEach((e, i) => {
+    e.addEventListener('keyup', (el) => {
+        if (e.value.length > 0) {
+            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(180deg)";
+        } else {
+            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(0deg)";
+        }
+    })
+})
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
