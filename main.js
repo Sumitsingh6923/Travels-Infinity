74ee392bc503572630c43282ccf6287e2aefa827
Sumitsingh6923 <125748987+Sumitsingh6923@users.noreply.github.com>
2025-07-13 21:26:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function closemenu() {
 }
 
 // -----------------REVIEW----------------------------
-var swiper = new Swiper(".swiper-slide", {
+var swiper = new Swiper(".review-slider", {
     spaceBetween: 20,
     loop: true,
     autoplay: {
