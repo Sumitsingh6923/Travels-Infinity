a1a9e9c18be8c00065dbc8694eb6a7741fccd12f
Sumitsingh6923 <125748987+Sumitsingh6923@users.noreply.github.com>
2025-07-13 21:13:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,23 +21,23 @@ function closemenu() {
 }
 
 // -----------------REVIEW----------------------------
-// var swiper = new Swiper(".swiper-slide", {
-//     spaceBetween: 20,
-//     loop: true,
-//     autoplay: {
-//         delay: 2500,
-//         disableOnInteraction: false,
-//     },
-//     breakpoints: {
-//         640: {
-//             slidesPerView: 1,
-//         },
-//         768: {
-//             slidesPerView: 2,
-//         },
-//         1024: {
-//             slidesPerView: 3,
-//         },
-//     },
-// });
+var swiper = new Swiper(".swiper-slide", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
 
